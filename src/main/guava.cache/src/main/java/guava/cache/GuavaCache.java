f0de0add6da9ddfe579ea29450163ac2ed89ddec
Andrei Klepets <ak1988.88@gmail.com>
2023-10-15 23:21:19 -0400

    cache improvements

--- src/main/guava.cache/src/main/java/guava/cache/GuavaCache.java
+++ src/main/guava.cache/src/main/java/guava/cache/GuavaCache.java
@@ -1,43 +1,45 @@
 package guava.cache;
 
 import com.google.common.cache.*;
+import service.cache.CacheEntry;
 
 import java.util.concurrent.TimeUnit;
 
 public class GuavaCache {
     public static void main(String[] args) throws InterruptedException {
-        Cache<String, Long> cache = CacheBuilder
+
+        RemovalListener<CacheEntry, Long> listener = new RemovalListener<>() {
+            int numberItemsEvicted = 0;
+            @Override
+            public void onRemoval(RemovalNotification<CacheEntry, Long> n){
+                if (n.wasEvicted()) {
+                    numberItemsEvicted++;
+                    CacheEntry key = n.getKey();
+                    Long value = n.getValue();
+                    String cause = n.getCause().name();
+                    System.out.println("key was: " + key + "value was: " + value + "cause was: "+ cause);
+                }
+                System.out.println("number of items evicted " + numberItemsEvicted);
+            }
+        };
+
+        Cache<CacheEntry, Long> cache = CacheBuilder
                 .newBuilder()
                 .maximumSize(100_000)
                 .expireAfterAccess(5, TimeUnit.SECONDS)
                 .expireAfterWrite(5, TimeUnit.SECONDS)
+                .removalListener(listener)
                 .build();
-
-        RemovalListener<String, String> listener = new RemovalListener<String, String>() {
-            int numberItemsEvicted = 0;
-            @Override
-            public void onRemoval(RemovalNotification<String, String> n){
-                if (n.wasEvicted()) {
-                    numberItemsEvicted++;
-                    String key = n.getKey();
-                    String value = n.getValue();
-                    String cause = n.getCause().name();
-                    System.out.println("key was: " + key + "value was: " + value + "cause was: "+ cause);
-                }
-            }
-        };
 
         long startTime = System.nanoTime();
         int itemCount = 200_000;
         for (int i = 0; i < itemCount; i++){
-            Thread.sleep(1);
-
-            cache.put("item" + i, System.currentTimeMillis());
+            cache.put(new CacheEntry("item" + i), System.currentTimeMillis());
         }
         long endTime = System.nanoTime();
-        double averageTime = endTime - startTime / itemCount;
+        double averageTime = (double) endTime - startTime / itemCount;
 
-        Long value = cache.getIfPresent("item199999");
+        Long value = cache.getIfPresent(new CacheEntry("item199999"));
         System.out.println(value);
         System.out.println("cache size is " + cache.size());
         System.out.println("averageTime to put one item is: " + averageTime);
--- src/main/service.cache/src/main/java/service/cache/CacheService.java
+++ src/main/service.cache/src/main/java/service/cache/CacheService.java
@@ -1,42 +1,61 @@
 package service.cache;
 
-import java.util.EventListener;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CacheService {
-    private static final int MAX_SIZE = 100;
+    private static final int MAX_SIZE = 100_000;
     private static final long EVICTION_TIME_MS = 5_000;
-    private Map<String, Long> cache;
-    private long totalPutTimeMiliSeconds;
+    private final Map<CacheEntry, Long> cache;
+    private long totalPutTimeMilliSeconds;
     private long evictionCount;
-
-    EventListener eventListener = new EventListener() {
-    };
 
     public CacheService() {
         cache = new ConcurrentHashMap<>(MAX_SIZE);
-        totalPutTimeMiliSeconds = 0;
+        totalPutTimeMilliSeconds = 0;
         evictionCount = 0;
     }
 
-    public void put(String s){
-//        evictEntries();
+    public void put(CacheEntry c){
         long startTime = System.currentTimeMillis();
-        cache.put(s, System.currentTimeMillis());
-        totalPutTimeMiliSeconds += System.currentTimeMillis() - startTime;
+        if (cache.size() == MAX_SIZE) {
+            evictOldestEntry();
+        }
+        evictOutdatedEntries();
+        cache.put(c, System.currentTimeMillis());
+        totalPutTimeMilliSeconds += System.currentTimeMillis() - startTime;
         System.out.println("cache size is: " + cache.size());
-        System.out.println("cache totalPutTime: " + totalPutTimeMiliSeconds / 1_000_000);
+        System.out.println("cache totalPutTime: " + totalPutTimeMilliSeconds / 1_000_000);
     }
 
-    public Long get(String key){
-        if (cache.keySet().contains(key)){
-            return cache.get(key);
+    public Long get(CacheEntry c){
+        if (cache.containsKey(c)){
+            if (System.currentTimeMillis() - cache.get(c) < EVICTION_TIME_MS)
+            {
+                return cache.get(c);
+            } else {
+                evictOutdatedEntries();
+            }
         }
         return null;
     }
 
-    private void evictEntries(){
+    private void evictOldestEntry(){
+        CacheEntry oldestElement = null;
+        long oldestTimestamp = Long.MAX_VALUE;
+
+        for (Map.Entry<CacheEntry, Long> entry : cache.entrySet()) {
+            long timestamp = entry.getValue();
+            if (timestamp < oldestTimestamp) {
+                oldestTimestamp = timestamp;
+                oldestElement = entry.getKey();
+            }
+        }
+        cache.entrySet().remove(oldestElement);
+        evictionCount++;
+    }
+
+    private void evictOutdatedEntries(){
         cache.entrySet().removeIf(entry -> {
             boolean isEvicted = System.currentTimeMillis() - entry.getValue() > EVICTION_TIME_MS;
             if (isEvicted) {evictionCount++;}
@@ -44,12 +63,10 @@ public class CacheService {
         });
     }
 
-
-
     public void printStatistics(){
         System.out.println("Cache statistics:");
         System.out.println("Cache size:" + cache.size());
-        System.out.println("average time spent putting new values into cache: " + totalPutTimeMiliSeconds + " miliseconds");
+        System.out.println("average time spent putting new values into cache: " + totalPutTimeMilliSeconds + " miliseconds");
         System.out.println("number of cache evictions: " + evictionCount);
     }
 
@@ -57,13 +74,12 @@ public class CacheService {
         CacheService cacheService = new CacheService();
 
         for (int i = 0; i < 200; i++){
-            Thread.sleep(100);
-            cacheService.put("item" + i );
+            cacheService.put(new CacheEntry("item" + i) );
         }
 
-        cacheService.cache.entrySet().stream().forEach(element -> element.toString());
+        cacheService.cache.entrySet().forEach(element -> element.toString());
 
-        long value = cacheService.get("item1");
+        long value = cacheService.get(new CacheEntry("item1"));
         System.out.println("Value for key item1 is " + value);
 
         cacheService.printStatistics();
